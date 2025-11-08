6686f967b172976cd0470e3228b240bf107f079f
hyeok <cod3holic@gmail.com>
2025-11-08 21:58:52 +0900

    boj7795

--- newboj/boj7795.cpp
+++ newboj/boj7795.cpp
@@ -1,4 +1,8 @@
+// map iteration 연습하기 좋은 문제
+
 #include<iostream>
+#include<map>
+#include<algorithm>
 
 using namespace std;
 
@@ -13,19 +17,47 @@ int main() {
         int n, m;
         cin >> n >> m;
 
-        map<int, int> arr1;
-        map<int, int> arr2;
+        map<int, int> a;
+        map<int, int> b;
+        unordered_map<int, int> psum;
         for(int i = 0; i < n; i++) {
             int k;
             cin >> k;
-            arr1[k] += 1;
+            a[k] += 1;
         }
         for(int i = 0; i < m; i++) {
             int k;
             cin >> k;
-            arr2[k] += 1;
+            b[k] += 1;
         }
 
-        
+        int full = 0;
+        int temp = 0;
+        for(auto k: b) {
+            full += k.second;
+            psum[k.first] = temp;
+            temp += k.second;
+        }
+
+        // for(auto k: psum) {
+        //     cout << k.first << " " << k.second << "\n";
+        // }
+
+        int value = 0;
+        for(auto now: a) {
+            auto lit = b.lower_bound(now.first);
+            if(lit != b.end()) {
+                // cout << now.first << " " << lit->first << " " << psum[lit->first] << " " << now.second << "\n";
+                value += psum[lit->first] * now.second;
+                // for(auto it = b.begin(); it != lit; it++) { // 최초시도: 이터레이팅.. 연습
+                //     value += now.second * it->second;
+                // }
+                // cout << now.first << " " << value << "\n";
+            }
+            else {
+                value += full * now.second;
+            }
+        }
+        cout << value << "\n";
     }
 }
